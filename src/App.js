f43837e74c8d0323d7dff93e4776a073e5079343
Javier Chamorro <javierchamorro611@gmail.com>
2020-06-04 21:01:47 +0200

    transformation from matching to multiplechoice with Aiken

--- src/App.js
+++ src/App.js
@@ -26,9 +26,7 @@ class App extends Component {
 
   
   render() {
-
-    this.matchingAikenToMultipleMoodleXML()
-
+    var opcion = "'#{option}'"
     const SpanishTooltip = props => (
       <Tooltip {...props}>Spanish description</Tooltip>
     );
@@ -46,7 +44,10 @@ class App extends Component {
     );
 
     const MatchingToMultipleChoiceTooltip = props => (
-      <Tooltip {...props}>Transform matching question to multiple choice</Tooltip>
+      <Tooltip {...props}>
+        <label>Transform matching question to multiple choice. It is necessary to include the text {opcion} for craeting the question properly</label>
+        <span className="font-weight-bold">The input matching question must be in Aiken format</span>
+      </Tooltip>
     );
 
     return (
@@ -121,6 +122,9 @@ class App extends Component {
                           </tr>
                         </tbody>
                     </table>
+                    <div className="text-center">
+                      <button onClick={this.insertNewQuestion.bind(this, "matching-to-multiplechoice")} type="button" className="btn btn-link">matching2multiplechoice</button>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -219,7 +223,7 @@ class App extends Component {
             <div><h2>To</h2>
             <select onChange={(e)=>{this.setState({to: e.target.value})}} value={this.state.to}>
               <option value="xml" >MoodleXML</option>
-              {/* <option disabled value="txt" >Aiken</option> */}
+              <option value="txt" >Aiken</option>
               <option value="json" >JSON</option>
             </select>
             </div>
@@ -241,8 +245,9 @@ class App extends Component {
     );
   }
   convert() {
-    const {from, to, left} = this.state;
+    var {from, to, left} = this.state;
     console.log(from, to, left)
+
     if (from === "xml" && to === "json") {
       moodleXMLtoJson(left, (res,err)=>{
         if (err) {
@@ -255,6 +260,9 @@ class App extends Component {
         this.setState({right});
       })
     } else if (from === "txt" && to === "json") {
+      if(this.state.matchingToMultiple){
+        left = this.findMatchigQuestionsAndChange(left);
+      }
       aikenToMoodleXML(left, (result, error)=>{
         console.log(result, error)
         moodleXMLtoJson(result.replace(/\t/g, "  "), (res,err)=>{
@@ -270,6 +278,9 @@ class App extends Component {
         })
       }, {lang: this.state.es ? "es": "en", penalty: this.state.penalty, nsnc: this.state.nsnc, shuffle: this.state.shuffle});
     } else if (from === "txt" && to === "xml") {
+      if(this.state.matchingToMultiple){
+        left = this.findMatchigQuestionsAndChange(left);
+      }
       aikenToMoodleXML(left, (res, err) => {
         if (err) {
           console.error(err);
@@ -283,6 +294,11 @@ class App extends Component {
           this.setState({right});
       }, {lang: this.state.es ? "es": "en", penalty: this.state.penalty, nsnc: this.state.nsnc, shuffle: this.state.shuffle});
     } else if (from === "xml" && to === "xml") {
+      this.setState({right: left});
+    }else if (from === "txt" && to === "txt"){
+      if(this.state.matchingToMultiple){
+        left = this.findMatchigQuestionsAndChange(left);
+      }
       this.setState({right: left});
     }
   }
@@ -324,38 +340,76 @@ class App extends Component {
     // window.Moodle = Moodle;
   }
 
-  matchingAikenToMultipleMoodleXML(){
-const matching =  `
-matching
-X. Match cities and countries
-1. Yakutsk
-match: Russia
-2. Tampere
-match: Finland
-3. Harbin
-match: China
-Feedback: Good job!
-`
-
-    var m = matching.split("\n");
-    //console.log(m);
-
-    var cnt = 1;
-    for (var i = 0;i < m.length;i++){      
-      if(m[i].includes(cnt.toString()+'.')&&m[i+1].includes("match")){
-        console.log(m[i]);
-        cnt ++;
+  findMatchigQuestionsAndChange(questions){
+    var t = questions.split("\n");    
+    var result = '';
+    var write = true;
+    var aux = '';
+    for (var i = 0;i<t.length;i++){
+      if(write && t[i].includes("matching")){
+        write = false
+      } else if (!write && (t[i].includes("matching") || t[i].includes("essay") || t[i].includes("shortanswer") || t[i].includes("truefalse") || t[i].includes("description") || t[i].includes("cloze") || t[i].includes("numerical") || t[i].includes("order") || t[i].includes("multichoice"))){
+        write = true
+        result = result + this.matchingAikenToMultipleMoodleXML(aux);        
+        aux = ''
+      }else if (!write && i+1 === t.length){
+        aux = aux + t[i];
+        write = false
+        result = result + this.matchingAikenToMultipleMoodleXML(aux);        
+        aux = ''
+      }
+      if(write){
+        result = result + t[i] + "\n";
+      }else{
+        aux = aux + t[i] +"\n";
       }
     }
+    console.log(result);
+    return result;
+    
+  }
+
+  matchingAikenToMultipleMoodleXML(matchingQuestion){
+    var m = matchingQuestion.split("\n");
+    
+    var cnt = 1;
+    var question = ['','']
+    var options = [];
+    var answers = [];
+    var feedback = "Good";
+    for (var i = 0;i < m.length;i++){
+      console.log(m[i]);
+      
+      if (m[i].includes('#{option}')){
+        question = m[i].split('#{option}');        
+      }
+      if(i+1 <= m.length && (m[i].includes(cnt.toString()+'.') && m[i+1].includes("match"))){        
+        options.push(m[i].split(cnt+'. ')[1]);
+        answers.push(m[i+1].split('match: ')[1]);
+        cnt ++;
+      }else if(m[i].includes("Feedback:")){
+        console.log("aaaa");
+        
+        feedback = m[i].split('Feedback: ')[1]
+      }
+    }
+
+    console.log(feedback);
     
 
     var result = ''
 
-    result = result+"multichoice\n"
-    result = result+"X. "
+    for ( var k = 0; k < options.length;k++){
+      result = result+"multichoice\n"
+      result = result+question[0]+options[k]+question[1]+"\n";
+      for(var j = 0; j < answers.length;j++){
+        result = result+String.fromCodePoint(65+j)+". "+answers[j]+"\n"
+      }
+      result = result+"Answer: "+String.fromCodePoint(65+k)+"\n"
+      result = result+"Feedback: "+feedback+"\n\n"
+    }
     
-    console.log(result);
-    
+    return result
   }
 
   aikenToMoodle(aikenText){
@@ -376,8 +430,7 @@ Feedback: Good job!
 
   insertNewQuestion(type){
 
-const multichoice =  `
-multichoice
+const multichoice =  `multichoice
 X. When an organization decides to control the flow of incident information within the IT organization, which ITIL process would it be putting in place?
 A. Availability Management
 B. Change Management
@@ -385,50 +438,50 @@ C. Incident Management
 D. Problem Management
 Answer: C, D
 gfeed. Duh!
+
 `
-const essay =  `
-essay
+const essay =  `essay
 X. The new role of social blogging in e-learning.
 gfeed. Write something about Twitter, Facebook from the aspects of teaching and colloboration.
+
 `
-const shortanswer =  `
-shortanswer
+const shortanswer =  `shortanswer
 X. Calculate: 2 + 2 
 Answer: 4, four
+
 `
 
-const truefalse =  `
-truefalse
+const truefalse =  `truefalse
 X. The founder of "Apple" was Steve Jobs.
 Answer: True
 Feedback: Steve Jobs is the CEO of Apple, which he co-founded in 1976.
+
 `
-const description =  `
-description
+const description =  `description
 X. Open Office is the alternative to Microsoft Office.
+
 `
 
-const cloze =  `
-cloze
+const cloze =  `cloze
 X. Infrastructure <{1:MULTICHOICE:=Monitoring~Controlling~Service} will provide support teams with alerts directly allowing for faster resolution. 
 Such alerts do not need to be recorded in the Incident Management tool as there is little added value in this {1:MULTICHOICE:=true~false}. 
 Typically the incident will be resolved automatically before the customer recognises it.
+
 `
-const numerical =  `
-numerical
+const numerical =  `numerical
 X. How many books are in ITIL V3? Answer only with a number.
 Answer: 5, 7
+
 `
-const order =  `
-order
+const order =  `order
 X. Place in ascending order
 1. 200
 2. 500
 3. 100
 ANSWER: 3,1,2
+
 `
-const matching =  `
-matching
+const matching =  `matching
 X. Match cities and countries
 1. Yakutsk
 match: Russia
@@ -437,6 +490,18 @@ match: Finland
 3. Harbin
 match: China
 Feedback: Good job!
+
+`
+const matching_to_multiplechoice =  `matching
+X. In which country is #{option}
+1. Yakutsk
+match: Russia
+2. Tampere
+match: Finland
+3. Harbin
+match: China
+Feedback: Good job!
+
 `
 
     switch (type) {
@@ -447,7 +512,6 @@ Feedback: Good job!
           this.setState({left : this.state.left + this.aikenToMoodle(multichoice)})
         }else{
           console.log("Error not possible state");
-          
         }
         break;
       case "essay":
@@ -457,7 +521,6 @@ Feedback: Good job!
           this.setState({left : this.state.left + this.aikenToMoodle(essay)})
         }else{
           console.log("Error not possible state");
-          
         }
         break;
       case "shortanswer":
@@ -467,7 +530,6 @@ Feedback: Good job!
           this.setState({left : this.state.left + this.aikenToMoodle(shortanswer)})
         }else{
           console.log("Error not possible state");
-          
         }
         break;
       case "truefalse":
@@ -477,7 +539,6 @@ Feedback: Good job!
           this.setState({left : this.state.left + this.aikenToMoodle(truefalse)})
         }else{
           console.log("Error not possible state");
-          
         }
         break;
       case "description":
@@ -487,7 +548,6 @@ Feedback: Good job!
           this.setState({left : this.state.left + this.aikenToMoodle(description)})
         }else{
           console.log("Error not possible state");
-          
         }
         break;
       case "cloze":
@@ -497,7 +557,6 @@ Feedback: Good job!
           this.setState({left : this.state.left + this.aikenToMoodle(cloze)})
         }else{
           console.log("Error not possible state");
-          
         }
         break;
       case "numerical":
@@ -507,7 +566,6 @@ Feedback: Good job!
           this.setState({left : this.state.left + this.aikenToMoodle(numerical)})
         }else{
           console.log("Error not possible state");
-          
         }
         break;
       case "order":
@@ -517,7 +575,6 @@ Feedback: Good job!
           this.setState({left : this.state.left + this.aikenToMoodle(order)})
         }else{
           console.log("Error not possible state");
-          
         }
         break;
       case "matching":
@@ -527,9 +584,17 @@ Feedback: Good job!
           this.setState({left : this.state.left + this.aikenToMoodle(matching)})
         }else{
           console.log("Error not possible state");
-          
         }
         break;
+        case "matching-to-multiplechoice":
+          if(this.state.from === "txt"){
+            this.setState({left : this.state.left + matching_to_multiplechoice})
+          }else if(this.state.from === "xml"){
+            this.setState({left : this.state.left + this.aikenToMoodle(matching_to_multiplechoice)})
+          }else{
+            console.log("Error not possible state");
+          }
+          break;
       default:
         console.log("Error with type", type);
         break;
